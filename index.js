c7a6f51747e8090da43afb5e084d9cf7d355b616
Nephew20 <cofieldk20@gmail.com>
2023-03-24 12:40:33 -0400

    added questions array

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = ['What is the title? ', 'Give a description of the project: ', 'What are the steps to install the project? ', 'Any Contribution guidelines? ', 'Any testing instructions? '];
+console.log(questions)
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
